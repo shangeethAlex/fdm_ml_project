fd875834e4ebc46d680bb3fa5658ad277a0d49e6
IT21809538 <111260593+IT21809538@users.noreply.github.com>
2024-09-28 19:10:05 +0530

    hhhh

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
 application=Flask(__name__)
-
+#fffff
 app=application
 
 ## Route for a home page
